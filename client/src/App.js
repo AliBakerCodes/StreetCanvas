3295b46a568be2a1f39a093cfa1bce2605aaa148
Mikhail25 <42049923+Mikhail25@users.noreply.github.com>
2022-08-06 14:37:41 -0400

    Made a react version of the template (No Bootstrap yet)

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,55 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// // import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-// import Upload from './pages/Upload';
-// import GetLocation from './pages/GetLocation';
-// import GoLive from './pages/GoLive';
-// import Explore from './pages/Explore';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Upload from './pages/Upload';
+import GetLocation from './pages/GetLocation';
+import GoLive from './pages/GoLive';
+import Explore from './pages/Explore';
+import Navigation from './pages/Navigation';
 
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
-// function App() {
-//   return (
-//     <ApolloProvider client={client}>
-//       <Router>
-//         <div className="flex-column justify-center align-center min-100-vh bg-primary">
-//           <Routes>
-//             <Route 
-//               path="/upload" 
-//               element={<Upload />}
-//             />
-//             <Route 
-//               path="/getlocation" 
-//               element={<GetLocation />}
-//             />
-//             <Route 
-//               path="/golive" 
-//               element={<GoLive />}
-//             />
-//             <Route 
-//               path="/explore"
-//               element={<Explore />}
-//             />
-//           </Routes>
-//         </div>
-//       </Router>
-//     </ApolloProvider>
-//   );
-// }
+function App() {
+  return (
+    // <ApolloProvider client={client}>
+    //   <Router>
+    //     <div className="flex-column justify-center align-center min-100-vh bg-primary">
+    //       <Routes>
+    //         <Route 
+    //           path="/upload" 
+    //           element={<Upload />}
+    //         />
+    //         <Route 
+    //           path="/getlocation" 
+    //           element={<GetLocation />}
+    //         />
+    //         <Route 
+    //           path="/golive" 
+    //           element={<GoLive />}
+    //         />
+    //         <Route 
+    //           path="/explore"
+    //           element={<Explore />}
+    //         />
+    //       </Routes>
+    //     </div>
+    //   </Router>
+    // </ApolloProvider>
+   <>
+        <Router>
 
-// export default App;
+            <Routes>
+                <Route path="/" element={<Navigation/>}>
+
+                </Route>
+            </Routes>
+        </Router>
+
+   </>
+  );
+}
+
+export default App;
