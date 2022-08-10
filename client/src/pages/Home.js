f486389654d5c55216fae9a824847b35ece7c345
AliBakercodes <alibakerconsulting@gmail.com>
2022-08-09 23:15:04 -0400

    Home.js 2 exports

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,3 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-
-
-export default function GoLive() {
-  return (
-    <div className="golive">
-      <h1>HOME</h1>
-
-    </div>
-  );
-}
-
-import React from "react";
-
 export default function Home() {
     return (
     <div>
