3c8e5bd996a88b0b174a347c95cb7d408555edfa
Mikhail25 <42049923+Mikhail25@users.noreply.github.com>
2022-08-09 23:50:18 -0400

    changed footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,63 +1,73 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Footer({ currentPage, handlePageChange }) {
   return (
     <Navbar fixed="bottom" className="footer mt-auto py-3 bg-light">
-      <div className="container row m-0 gx-3">
-      <div className="col">
-            <Button
-            type="button p-2"
-              href="#upload"
-              onClick={() => handlePageChange("Upload")}
-              className={`btn btn-outline-success btn-circle btn-md ${
-                currentPage === "Upload" ? "nav-link active" : "nav-link"
-              }`}
-            >
-              Upload
-            </Button>
-        </div>
+      <Container fluid='sm'>
+        <Row>
+          <Col>
+                <Button
+                type="button p-2"
+                variant="outline-success"
+                  href="#upload"
+                  onClick={() => handlePageChange("Upload")}
+                  className={`btn btn-outline-success btn-circle btn-md ${
+                    currentPage === "Upload" ? "nav-link active" : "nav-link"
+                  }`}
+                >
+                  Upload
+                </Button>
+          </Col>
 
-        <div className="col">
-            <Button
-            type="button p-2"
-              href="#addPeg"
-              onClick={() => handlePageChange("AddPeg")}
-              className={`btn btn-outline-success btn-circle btn-md ${
-                currentPage === "AddPeg" ? "nav-link active" : "nav-link"
-              }`}
-            >
-              Add Peg
-            </Button>
-        </div>
+          <Col>
+              <Button
+              type="button p-2"
+              variant="outline-success"
+                href="#addPeg"
+                onClick={() => handlePageChange("AddPeg")}
+                className={`btn btn-outline-success btn-circle btn-md ${
+                  currentPage === "AddPeg" ? "nav-link active" : "nav-link"
+                }`}
+              >
+                Add Peg
+              </Button>
+          </Col>
 
-        <div className="col">
-            <Button
-            type="button p-2"
-              href="#goLive"
-              onClick={() => handlePageChange("GoLive")}
-              className={`btn btn-outline-success btn-circle btn-md ${
-                currentPage === "GoLive" ? "nav-link active" : "nav-link"
-              }`}
-            >
-              Go Live
-            </Button>
-        </div>
+          <Col>
+              <Button
+              type="button p-2"
+              variant="outline-success"
+                href="#goLive"
+                onClick={() => handlePageChange("GoLive")}
+                className={`btn btn-outline-success btn-circle btn-md ${
+                  currentPage === "GoLive" ? "nav-link active" : "nav-link"
+                }`}
+              >
+                Go Live
+              </Button>
+          </Col>
 
-        <div className="col">
-            <Button
-            type="button p-2"
-              href="#explore"
-              onClick={() => handlePageChange("Explore")}
-              className={`btn btn-outline-success btn-circle btn-md ${
-                currentPage === "Explore" ? "nav-link active" : "nav-link"
-              }`}
-            >
-              Explore
-            </Button>
-        </div>
-      </div>
+          <Col>
+                <Button
+                type="button p-2"
+                variant="outline-success"
+                  href="#explore"
+                  onClick={() => handlePageChange("Explore")}
+                  className={`btn btn-outline-success btn-circle btn-md ${
+                    currentPage === "Explore" ? "nav-link active" : "nav-link"
+                  }`}
+                >
+                  Explore
+                </Button>
+          </Col>
+        </Row>
+
+      </Container>
 </Navbar>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
     <div>
-      <h1 className ="text-center">Welcome to Street Canvas</h1>
+      <h2 className ="text-center">Welcome to Street Canvas</h2>
     </div>
 
 
