441322c388d329ef258795148fb5db1a70cd72be
AliBakercodes <alibakerconsulting@gmail.com>
2022-08-11 19:44:48 -0400

    remove hashes so links work

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -35,21 +35,21 @@ function Header(){
     return(
 <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Street Canvas</Navbar.Brand>
+        <Navbar.Brand href="/">Street Canvas</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link href="#library">Library</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/library">Library</Nav.Link>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item href="action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="action/3.2">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="action/3.4">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
