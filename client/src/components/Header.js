c4db918bcce2db97f9b3176bdbbcb7e41623b1fc
ianjustinferris <ianjustinferris@gmail.com>
2022-08-12 13:22:57 -0400

    geolocation hook

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Placeholder from 'react-bootstrap/Placeholder';
 
 // function Header(){
@@ -32,15 +32,15 @@ import Placeholder from 'react-bootstrap/Placeholder';
 
 // export default Header;
 
-function Header(){
-    return(
-<Navbar as="p" animation="glow" bg="light" expand="lg">
+function Header() {
+  return (
+    <Navbar bg="light" expand="lg">
       <Container >
         <Navbar.Brand as={Link} to="/">Street Canvas</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto justify-content-center align-items-center" xs={12}>
-            <Nav.Link  as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/login">Login</Nav.Link>
             <Nav.Link as={Link} to="/library">Library</Nav.Link>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
@@ -58,7 +58,7 @@ function Header(){
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    )
+  )
 }
 
 export default Header;
--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -1,57 +1,42 @@
 import { Popup } from 'react-map-gl';
-import Map, { Marker } from 'react-map-gl';
+import ReactMapGL, { GeolocateControl, Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import React, { useState, useEffect } from 'react';
 import Footer from "../components/Footer";
+import useGeolocation from "../components/geoLocation";
 
 
 function Explore() {
-  const [pegs, setPegs] = useState([])
-  useEffect(() => {
-    const getPegs = async () => {
-      try {
-        const res = await axios.get("/pegs")
-        setPegs(res.data)
-      } catch (err) {
-        console.log('error in useEffect')
-      }
-    };
-    getPegs();
-  }, [])
+  const location = useGeolocation()
+  // <GeolocateControl
+  //   style={geolocateControlStyle}
+  //   positionOptions={{ enableHighAccuracy: true }}
+  //   trackUserLocation={true}
+  //   onGeolocate={(position) => {
+  //     // get latitude and longitude of user current location
+  //     setNewLocation([position.coords.latitude, position.coords.longitude]);
+  //   }}
+  // />
+
+  // const [viewport, setViewport] = useState({
+  //   latitude
+  // })
+
   return (
     <>
-      <Map
+      <ReactMapGL
         initialViewState={{
-          longitude: -100,
-          latitude: 40,
+          longitude: location.lon,
+          latitude: location.lat,
           zoom: 3.5
         }}
         style={{ width: '100vw', height: '100vh' }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken='pk.eyJ1IjoiaWFuanVzdGluZmVycmlzIiwiYSI6ImNsNmdxNmRkNDA2Z2IzaW53ZmZoZDU0NHgifQ.Pcg0JsdtFLHvd2EWFI4_mA'
       >;
-        {pegs.map(p => (
-          <>
-            <Marker
-              longitude={p.lon}
-              latitude={p.lat}
-              anchor="bottom" >
 
-              <PlaceIcon />
-            </Marker>
-            <Popup longitude={p.lon} latitude={p.lat}
-              anchor="top"
-            >
-              <div className="card">
-                <label className='userName'>USERNAME:</label>
-                <label className='title'>TITLE:</label>
-                <img className='thumbnail' src='' alt='robot' />
-                <label className='lat/lon'>LAT/LON:</label>
-              </div>
-            </Popup>
-          </>
-        ))}
-      </Map>
+      </ReactMapGL>
+      <div>{location.loaded ? JSON.stringify(location) : "Location data not available yet."}</div>
       <Footer />
     </>)
 };
