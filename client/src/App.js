900015166b04a7ca0fc021e6924f5c8ca45eda87
Erica Breig <ericabreig@gmail.com>
2022-08-12 01:28:58 -0400

    colored the buttons lol

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../src/pages/style.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   ApolloClient,
