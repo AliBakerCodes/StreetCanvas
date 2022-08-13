ec44a0969554ecbd255fb515ff5711373ef9d1d0
Erica Breig <ericabreig@gmail.com>
2022-08-13 10:12:43 -0400

    changed default route back to home

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,7 @@ function App() {
             <Route 
               path="/" 
 
-              element={<Explore />}
+              element={<Home />}
             />
             <Route
               path="/login"
