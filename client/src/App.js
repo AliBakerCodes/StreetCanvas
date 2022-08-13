5525dc953161e210f9adf9b56ce01af27d60e779
Erica Breig <ericabreig@gmail.com>
2022-08-13 05:56:13 -0400

    changed home page route to explore

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,7 @@ function App() {
             <Route 
               path="/" 
 
-              element={<Home />}
+              element={<Explore />}
             />
             <Route
               path="/login"
