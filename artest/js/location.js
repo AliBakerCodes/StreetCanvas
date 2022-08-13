60cc6b48ebd3c139162dc14d144ad99c9cc30e7c
Ali Baker <100386641+AliBakerCodes@users.noreply.github.com>
2022-08-13 10:06:31 -0400

    x

--- artest/js/location.js
+++ artest/js/location.js
@@ -19,7 +19,19 @@ if (typeof Number.prototype.toRad === "undefined") {
     return (this * Math.PI) / 180;
   };
 }
+const myUser = new user("Ali");
+myUser["models"] = [];
 
+const astro = new model("Astronaught", "https://purple-aardvark.s3.amazonaws.com/assets/3dmodels/NeilArmstrong.glb", "https://purple-aardvark.s3.amazonaws.com/assets/3dmodels/NeilArmstrong.usdz", "https://purple-aardvark.s3.amazonaws.com/assets/img/NeilArmstrong.png");
+myUser.models.push(astro);
+const witch = new model("Fire Witch", "https://purple-aardvark.s3.amazonaws.com/assets/3dmodels/Fire_Witch.glb", "https://purple-aardvark.s3.amazonaws.com/assets/3dmodels/Fire_Witch.usdz","https://purple-aardvark.s3.amazonaws.com/assets/img/Fire_Witch.png");
+myUser.models.push(witch);
+const baphomet = new model("Red Baphomet", "https://purple-aardvark.s3.amazonaws.com/assets/3dmodels/Red_Baphomet.glb","https://purple-aardvark.s3.amazonaws.com/assets/3dModels/Red_Baphomet.usdz", "https://purple-aardvark.s3.amazonaws.com/assets/img/Red_Baphomet.png")
+myUser.models.push(baphomet);
+const Genshin_Impact_Eula = new model("Genshin Impact - Eula", "https://purple-aardvark.s3.amazonaws.com/assets/3dmodels/Genshin_Impact_-_Eula.glb","https://purple-aardvark.s3.amazonaws.com/assets/3dmodels/Genshin_Impact_-_Eula.usdz", "https://purple-aardvark.s3.amazonaws.com/assets/img/genshin_impact_-_eula.png")
+myUser.models.push(Genshin_Impact_Eula);
+
+console.log("myUser", myUser);
 function updateItems() {
   document.getElementById("updateContainer").style.display = "none";
   document.getElementById("ar-button").style.display = "block";
